55ba66b21f7057a47b007b283092bb5b6c96bc9a
dhdjSYS <dhdjwjy@163.com>
2018-01-15 20:15:02 -0600

    不错,修复了网络协议,开始Link加密

--- modules/Network.php
+++ modules/Network.php
@@ -9,13 +9,14 @@ class Network{
     public function __construct(){
 
     }
-    public function setMP($ip,$port,$interval,$worker_num,$max_request,$Logger){
+    public function setMP($ip,$port,$interval,$worker_num,$max_request,$Logger,$Encrypt){
         $this->ip = $ip;
         $this->port = $port;
         $this->interval = $interval;
         $this->worker_num = $worker_num;
         $this->max_request= $max_request;
         $this->Logger=$Logger;
+        $this->Encrypt=$Encrypt;
         $this->serv = new swoole_server($this->ip, $this->port);
         $this->serv->set(array(
             'worker_num' => $this->worker_num,
@@ -32,6 +33,7 @@ class Network{
     public function response($serv,$fd,$from_id,$data){
             //响应行
             $respData = 'Welcome!!!!';
+            $respData = $this->Encrypt->Encode($respData);
             $response = array(
                 'HTTP/1.1 200',
             );
@@ -50,7 +52,7 @@ class Network{
             $response[] = $respData;
             $send_data = join("\r\n",$response);
             $serv->send($fd, $send_data);
-        $this->Logger->PrintLine("收到一条消息,from_id:".$from_id."data:".$data);
+        $this->Logger->PrintLine("收到一条http请求,from_id:".$from_id);
 
     }
     public function StartWeb(){
