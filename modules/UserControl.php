cdd5cf17f6012d5bc15072bb8e627640c4715707
dhdjSYS <dhdjwjy@163.com>
2018-01-18 19:55:13 -1000

    修复了一个问题

--- modules/UserControl.php
+++ modules/UserControl.php
@@ -24,19 +24,28 @@ class UserControl{
         return json_encode(array("USERDATADIR"=>$DIR));
     }
     public function NewUser($account,$password){
-        @mkdir($this->DIR.$account."/");
-        file_put_contents($this->DIR.$account."/password",md5($password));
-        if(@file_get_contents($this->DIR.$account."/password") == md5($password)){
-            return true;
-        }
-        return false;
-    }
-    public function DeleteUser($account){
-        unlink($this->DIR.$account."/password");
-        @rmdir($this->DIR.$account."/");
-        if(!file_exists($this->DIR.$account."/password")){
+        if(is_dir($this->DIR.$account."/")) {
+            @mkdir($this->DIR . $account . "/");
+            file_put_contents($this->DIR . $account . "/password", md5($password));
+            if (@file_get_contents($this->DIR . $account . "/password") == md5($password)) {
+                $this->Logger->PrintLine("成功添加新用户,用户信息:" . json_encode(array("account" => $account, "password" => $password), 0));
+                return true;
+            }
+            $this->Logger->PrintLine("失败添加新用户,用户信息:" . json_encode(array("account" => $account, "password" => $password), 2));
+            return false;
+        }else{
+            $this->Logger->PrintLine("正在试图添加一个已经存在的用户",5);
             return false;
         }
+    }
+    public function DeleteUser($account){
+        @unlink($this->DIR.$account."/password");
+        @rmdir($this->DIR.$account."/");
+        if(!file_exists($this->DIR.$account."/password")){
+            $this->Logger->PrintLine("失败删除老用户,用户信息:".json_encode(array("account"=>$account),0));
+            return false;
+        }
+        $this->Logger->PrintLine("成功删除老用户,用户信息:".json_encode(array("account"=>$account),0));
         return true;
     }
     public function Verify($account,$password){
