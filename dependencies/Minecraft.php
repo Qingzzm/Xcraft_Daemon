011a872aa41aeac9a5e61be851db123a47cf015a
dhdjSYS <dhdjwjy@163.com>
2018-02-02 16:45:57 +0800

    我肯定是智障

--- dependencies/Minecraft.php
+++ dependencies/Minecraft.php
@@ -20,6 +20,6 @@ class Minecraft extends Thread{
         if($this->DoShutDown){
             die("[意外]:dhdj!你又写bug!,赶紧给我修复了!!!\r\n");
         }
-        $this->Logger->PrintLine("看似成功的开启测试,id:".$this->id."data:".$this->data);
+        $this->Logger->PrintLine("看似成功的开启测试,id:".$this->id."data:".json_encode($this->data));
     }
 }
