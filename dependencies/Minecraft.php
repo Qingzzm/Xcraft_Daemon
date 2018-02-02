c4f18ad78788813a858abfbc958c9e8ac06bb311
dhdjSYS <dhdjwjy@163.com>
2018-02-02 16:31:24 +0800

    理论来讲可以修复

--- dependencies/Minecraft.php
+++ dependencies/Minecraft.php
@@ -8,17 +8,17 @@
 class Minecraft extends Thread{
     public function __construct($data,$Logger){
         if($data != false){
-            $this->data = $data;
-            $this->id= $this->data["id"];
+            $this::data = $data;
+            $this::id= $this::data["id"];
         }else{
             $this->DoShutDown=1;
         }
         $this->Logger = $Logger;
     }
     public function run(){
-        if($this->DoShutDown){
+        if($this::DoShutDown){
             die("[意外]:dhdj!你又写bug!,赶紧给我修复了!!!\r\n");
         }
-        $this->Logger->Printline("看似成功的开启测试,id:".$this->id."data:".$this->data);
+        $this::Logger->Printline("看似成功的开启测试,id:".$this::id."data:".$this::data);
     }
 }
