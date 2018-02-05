86e33f2586df339ce72f204ac4db31b818e5e93b
Teaclon <3385815158@qq.com>
2018-02-05 16:55:53 +0100

    lalala

--- modules/MySQL.php
+++ modules/MySQL.php
@@ -5,18 +5,29 @@
  * Date: 2/5/18
  * Time: 10:23 PM
  */
-class MySQL{
-    public function __construct(){
+class MySQL
+{
+    public function __construct()
+	{
 
     }
-    public function SetMP($Logger,$Host,$User,$Pass){
+	
+    public function SetMP($Logger,$Host,$User,$Pass)
+	{
         $this->Logger = $Logger;
         $this->Host = $Host;
         $this->User = $User;
         $this->Pass = $Pass;
         return json_encode(array("Host"=>$this->Host,"User"=>$this->User,"Pass"=>$this->Pass));
     }
-    public function NewUser($account,$password){
+	
+	public function GetServerInfoByID($id)
+	{
+		
+	}
+	
+    public function NewUser($account,$password)
+	{
         /*这里的MySQL操作自己写
         就是添加一个User23333
         如果不能登录或者出现了什么问题,那么这样调用Logger返回:$this->Logger->PrintLine("无法连接至MySQL",5);
@@ -24,4 +35,9 @@ class MySQL{
         */
 
     }
+	
+	public function GetCurrentServerID()
+	{
+		
+	}
 }
